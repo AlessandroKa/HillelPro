0831c74b6776d0c6ff8369b77defb73fefe172b9
unknown <sasha1804sk@gmail.com>
2022-07-04 04:30:36 +0300

    add task 2

--- Lesson 1/script.js
+++ Lesson 1/script.js
@@ -1,4 +1,14 @@
-let count = 7;
+//task 1 (Calculator)
+
+const count = 7;
 console.log('Значение: ' + count);
 console.log('Квадрат этого значения: ' + count ** 2);
 console.log('Куб этого значения: ' + count ** 3);
+
+//task 2 (temperature converter)
+
+const tempCelsium = 25;
+let tempFahrenheit = tempCelsium * 9 / 5 + 32;
+console.log(tempCelsium + '\u00B0C ' + 'равно ' + tempFahrenheit + '\u00B0F')
+let editCelsium = ((tempFahrenheit - 32) * 5) / 9
+console.log(tempFahrenheit + '\u00B0F ' + 'равно ' + editCelsium + '\u00B0C')
