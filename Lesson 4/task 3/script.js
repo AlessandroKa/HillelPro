da7c0ca8b8488e4ffae733ea134b8aca57c3fcc9
unknown <sasha1804sk@gmail.com>
2022-07-16 02:53:44 +0300

    lesson4

--- Lesson 4/task 3/script.js
+++ Lesson 4/task 3/script.js
@@ -1,9 +1,9 @@
 // Вариант первый простой как 5 копеек, но эффективный :)
 
-// let word = prompt('Write your word here');
-// let letter = prompt('Write the letter you want to find the amount of');
-// let findLetter = (word.split(letter).length - 1);
-// console.log(`The string contains the following number of ${letter} letter: ${findLetter}`);
+let word = prompt('Write your word here');
+let letter = prompt('Write the letter you want to find the amount of');
+let findLetter = (word.split(letter).length - 1);
+console.log(`The string contains the following number of ${letter} letter: ${findLetter}`);
 
 // Вариант второй посложнее, но зато используем цикл
 
@@ -12,7 +12,8 @@ let letter = prompt('Write the letter you want to find the amount of');
 let y = 0;
 
 for (let i = 0; i < word.length; i++) {
-    word[i] === letter ? y++ :        
+    if (word[i] === letter) {
+        y++;
     }
-
+}
 console.log(`The string contains the following number of ${letter} letter: ${y}`);
